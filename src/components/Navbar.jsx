8aae9b0a98340656fb06ad83428c11d93c4f1368
Karthik-prakash18 <prakash.kart@northeastern.edu>
2024-08-15 01:52:04 -0400

    Navbar and About just started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link as LinkR} from "react-router-dom";
-import styled from 'styled-components';
+import styled, {useTheme} from 'styled-components';
 import {Bio} from "../data/constants";
+import {MenuRounded} from "@mui/icons-material";
 
-const NavbarContainer = styled.div`
-    background-color: ${({theme}) => theme.bg};
+const Nav = styled.div`
+    background-color: ${({ theme }) => theme.bg};
     height: 80px;
     display: flex;
     align-items: center;
@@ -14,9 +15,22 @@ const NavbarContainer = styled.div`
     top: 0;
     z-index: 10;
     color: white;
+`
+
+const NavbarContainer = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 24px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 1rem;
 `;
 const NavLogo = styled(LinkR)`
+    width: 80%;
     padding: 0 6px;
+    font-weight: 500;
+    font-size: 18px;
     text-decoration: none;
     color: inherit;
 `;
@@ -32,7 +46,7 @@ const NavItems = styled.ul`
     @media screen and (max-width: 768px) {
         display: none;
     }
-`
+`;
 
 const NavLink = styled.a`
     color: ${({theme}) => theme.text_primary};
@@ -43,7 +57,7 @@ const NavLink = styled.a`
     &:hover{
         color: ${({theme}) => theme.primary};
     }
-`
+`;
 
 const ButtonContainer = styled.div`
     width: 80%;
@@ -55,7 +69,7 @@ const ButtonContainer = styled.div`
     @media screen and (max-width: 768px) {
         display: none;
     }
-`
+`;
 
 const GithubButton = styled.a`
     border: 1px solid ${({theme}) => theme.primary};
@@ -74,14 +88,57 @@ const GithubButton = styled.a`
         background-color: ${({theme}) => theme.primary};
         color: ${({theme}) => theme.text_primary}
     }
-`
+`;
+
+const MobileIcon = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    color: ${({theme}) => theme.text_primary};
+    display: none;
+    @media screen and (max-width: 768px) {
+        display: block;
+    }
+`;
+
+const MobileMenu = styled.ul`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    gap: 16px;
+    padding: 0 6px;
+    list-style: none;
+    width: 100%;
+    padding: 12px 40px 24px 40px;
+    background: ${({theme}) => theme.card_light + 99};
+    position: absolute;
+    top: 80px;
+    right: 0;
+
+    transition: all 0.6s ease-in-out;
+    transform: ${({ isOpen }) =>
+        isOpen ? "translateY(0)" : "translateY(-100%)"};
+    border-radius: 0 0 20px 20px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+    z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+`;
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const theme = useTheme();
   return (
-  <NavbarContainer>
+  <Nav>
+    <NavbarContainer>
     <NavLogo to="/">
         GeekForGeeks
     </NavLogo>
+
+    <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+        <MenuRounded style={{color: "inherit"}} />
+    </MobileIcon>
 
     <NavItems>
         <NavLink href="#About">About</NavLink>
@@ -91,10 +148,27 @@ const Navbar = () => {
         <NavLink href="#Education">Education</NavLink>
     </NavItems>
 
+    {
+        isOpen && (
+        <MobileMenu isOpen={isOpen}>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#About">About</NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Skills">Skills</NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Experience">Experience</NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Project">Project</NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Education">Education</NavLink>
+            <GithubButton href={Bio.github} target="_blank" style={{
+                background: theme.primary,
+                color: theme.text_primary
+            }}>Github Profile</GithubButton>
+        </MobileMenu>
+    )}
+
     <ButtonContainer>
         <GithubButton href={Bio.github} target="_blank">Github Profile</GithubButton>
     </ButtonContainer>
   </NavbarContainer>
+  </Nav>
+  
   );
 };
 
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from "styled-components";
+const Hero = () => {
+  return (
+    <div id="about">
+      Hero
+    </div>
+  )
+}
+
+export default Hero
