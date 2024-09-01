5e86aa294455a627683bc5c4ad38f1b595ecedbf
Karthik-prakash18 <prakash.kart@northeastern.edu>
2024-09-01 10:22:03 -0700

    Added experience section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import Hero from "./components/sections/Hero";
 import Skills from "./components/sections/Skills";
+import Experience from "./components/sections/Experience";
 
 const Body = styled.div`
   background-color: ${({theme}) => theme.bg};
@@ -23,6 +24,7 @@ function App() {
       <Body>
         <Hero />
         <Skills />
+        <Experience />
       </Body>
     </BrowserRouter>
   </ThemeProvider>
